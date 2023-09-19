caa44d260a86171b8bc381b28c7b42066f0db7c6
orange <anlewong@ucsc.edu>
2023-09-19 11:08:34 -0700

    added more interaction with stat generator to see rolls and removed roll

--- stats.py
+++ stats.py
@@ -5,7 +5,14 @@ col, row = 4, 6
 if inp == "y":
     raw_stats = [[random.randrange(1,6) for i in range(col)] for j in range(row)]
     fin_stats = [(sum(raw_stats[i])-min(raw_stats[i])) for i in range(row)]
-    print(fin_stats)
+    if "n" == input("see rolls? (y/n)"):
+        print(fin_stats)
+    else:
+        for i in range(row):
+            stats = raw_stats[i]
+            print("Stat Value: " + str(sum(stats)) + "\nrolls " + str(stats) + " removed lowest roll " + str(min(stats)) + "\n")  
+
+            
+    
 else:
     print([15,14,13,12,10,8]) 
-    
